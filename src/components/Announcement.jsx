bb4b7e04bf465a96f460b8f8fd4e72c7abb80dd5
abdu <sampv0166@gmail.com>
2022-08-24 02:39:15 +0400

    created menu items for the nav bar, applied more styles to search box, created the announcement component

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container  = styled.div`
+    height: 30px;
+    background-color: teal;
+    color: white;
+`
+
+
+const Announcement = () => {
+  return (
+    <div>Announcement</div>
+  )
+}
+
+export default Announcement
