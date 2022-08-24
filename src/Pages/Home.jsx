de59ab79691b99f4a426132e1b40445c1d7c1e6a
abdu <sampv0166@gmail.com>
2022-08-24 18:55:30 +0400

    complted slider, products component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,13 +2,21 @@ import React from 'react'
 import Announcement from '../components/Announcement'
 import Navbar from '../components/Navbar'
 import Slider from '../components/Slider'
+import Products from '../components/Products'
 
 const Home = () => {
   return (
     <div>
     <Announcement/>
     <Navbar/>
-    <Slider/>
+    <Slider/> 
+   <Products/>
+   {/* <Footer/> */}
+  {/*
+    <Categories/>
+    <Newsletter/>
+    <Footer/>
+  */}
     </div>
   )
 }
